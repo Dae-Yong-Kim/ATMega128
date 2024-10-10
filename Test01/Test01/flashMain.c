ae539a0c2d07ba1c8ce3159f5ba7306117a85984
Dae-Yong-Kim <youbinlove607@gmail.com>
2024-10-10 11:17:23 +0900

    edit some comments

--- Test01/Test01/flashMain.c
+++ Test01/Test01/flashMain.c
@@ -4,7 +4,7 @@
  * Created: 2024-10-02 오후 12:17:19
  * Author : user
  */ 
-#define F_CPU 16000000UL // 16MHz | delay 사용시 필요
+#define F_CPU 16000000UL // 16MHz | 반드시 넣어주자 안넣으면 default로 1G로 문제가 생김
 #include <avr/io.h>
 #include <util/delay.h>
 //  SW : PG3, Input
@@ -36,7 +36,7 @@ int main(void)
     /* Replace with your application code */
 	//printf("Hello World!");
 	DDRG |= 0x10; // 4번 (0~4) | xxxx xxxx ==> xxx1 xxxx | 0=입력 1=출력 | G4번 핀을 출력으로 만들었다.
-	DDRG &= ~0x0e; // 3번 (0~4) | xxxx xxxx ==> xxxx 000x | 0=입력 1=출력 | G3번 핀을 입력으로 만들었다.
+	DDRG &= ~0x0e; // 1, 2, 3번 (0~4) | xxxx xxxx ==> xxxx 000x | 0=입력 1=출력 | G3번 핀을 입력으로 만들었다.
 	//DDG4 = 1; // bit에 직접 접근, but Const (상수) = Read Only ==> 0x10 = 1과 같다. (Error)
 	
 	//char v; // 변수를 while 밖에서 선언
@@ -63,7 +63,7 @@ int main(void)
 		//PORTG |= 0x10; // G4번 핀에 1을 내보낼거다.
 		
 		/*
-		########################## G4 pin에 500ms 간격으로 LED On Off - 1 ##########################
+		########################## G4 pin에 200ms 간격으로 LED On Off - 1 ##########################
 		*/
 		//PORTG |= 0x10;
 		//_delay_ms(200);
@@ -71,7 +71,7 @@ int main(void)
 		//_delay_ms(200);
 		
 		/*
-		########################## G4 pin에 500ms 간격으로 LED On Off - 2 ##########################
+		########################## G4 pin에 200ms 간격으로 LED On Off - 2 ##########################
 		*/
 		//if (PORTG & 0x10) // LED가 켜져있다면
 		//{
