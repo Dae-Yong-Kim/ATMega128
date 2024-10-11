3d80293581ca0d357c3c332d4b1c5b57383cc6e8
Dae-Yong-Kim <youbinlove607@gmail.com>
2024-10-11 14:49:22 +0900

    Make Standby Function to Library

--- Test01/Test02-GPIO/GPIOmain.c
+++ Test01/Test02-GPIO/GPIOmain.c
@@ -10,6 +10,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+
 #define LED1 PORTG3 // led1 (PORTG3 == 3)
 #define SW1 PORTD0 // switch1 (PORTD0 == 0)
 #define SW2 PORTD1 // switch2 (PORTD1 == 1)
@@ -25,21 +26,6 @@ ISR(INT0_vect){ // 느리게
 ISR(INT1_vect){ // 빠르게
 	ival -= 100;
 	if (ival < 0) ival = 0;
-}
-
-int TestBit(char pin, char mask) { // PINx 레지스터의 값의 mask bit가 0인지 1인지 판별
-	if((pin & mask) != 0) return 1;
-	return 0;
-}
-
-void StandBy() { // PG4 pin으로 프로그램 시작 스위치 연결
-	DDRG &= ~_BV(SW_STANDBY); // == "DDRG &= ~0x10;" | G4 입력
-	PORTG |= _BV(SW_STANDBY); // == "PORTG |= 0X10;" | G4 내부저항 Pull Up
-	
-	// PINx의 초기값은 N/A이다.
-	// 5V가 연결되어 있어도 처음 기기가 켜질 때는 1임을 보장할 수 없다. 따라서 stand-by 전에 PING1이 1임을 확인하고 들어가자
-	while(!TestBit(PING, 0x10)); // == "while((PING & _BV(SW_STANDBY)) == 0);" == "while((PING & 0X10) == 0);" == "while(1) { if((PING & 0X10) != 0) break; }" == "while(1) { if((PING & 0X10) == 0x10) break; }"
-	while(TestBit(PING, 0x10)); // == "while(!((PING & _BV(SW_STANDBY)) == 0));" == "while(!((PING & 0X10) == 0));" == "while(!((PING & (1 << SW_STANDBY)) == 0));" == "while(1) { if(PING & 0X10 == 0) break; }"
 }
 
 int main(void)
