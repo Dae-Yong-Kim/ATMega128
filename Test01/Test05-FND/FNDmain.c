846a1d497719bd4b6054d04b2027f599cf82bb01
Dae-Yong-Kim <youbinlove607@gmail.com>
2024-10-17 16:33:14 +0900

    Making Stop-Watch

--- Test01/Test05-FND/FNDmain.c
+++ Test01/Test05-FND/FNDmain.c
@@ -8,6 +8,7 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
 #define IDDR    DDRA
 #define IPORT   PORTA
@@ -15,42 +16,136 @@
 #define CPORT   PORTB
 
 unsigned char img[] = { 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x27, 0x7F, 0x67 };
-int deciDigit = 456;
 char data[4];
+int num = 0, st = 0, cnt = 0; // st == 0: reset | 1: start | 2 : stop
+
+ISR(INT0_vect) {
+	switch(st) {
+		case 0:	st = 1; break;
+		case 1:	st = 2; break;
+		case 2:	st = 0; num = 0; break;
+	}
+}
+
+ISR(TIMER0_COMP_vect) { // Timer용 ((0.01 / 7)s 단위)
+	cnt++;
+	if(cnt == 7) {
+		if(st == 1) {
+			num++;
+		}
+		digit(num);
+		cnt = 0;
+	}
+	TCNT0 = 0;
+}
+
+int tcnt = 0;
+ISR(TIMER2_OVF_vect) { // FND refresh (4ms 단위)
+	if(tcnt++ < 1) {
+		for (int i = 0; i < 4; i++) {
+#ifdef FND_TYPE_CATHOD
+			CPORT = ~(1 << i);
+			IPORT = img[data[i]];
+			if (i == 1) {
+				IPORT |= 0x80;
+			}
+#else
+			CPORT = 1 << i;
+			IPORT = ~img[data[i]];
+			if (i == 1) {
+				IPORT &= ~0x80;
+			}
+#endif
+		_delay_ms(1);
+		tcnt = 0;
+		}
+	}
+}
+
+int digit(int num) { // num 변수의 각자리수 추출하여 data[] 배열에 저장
+	if(num > 9999) return 0; // 인수 유효성 Check
+	// data[0] = (num / 1000) % 10;
+	data[0] = num / 1000;
+	data[1] = (num / 100) % 10;
+	data[2] = (num / 10) % 10;
+	data[3] = num % 10;
+	return 1;
+}
 
 int main(void)
 {
-	StandBy();
 	CDDR |= 0x0F;
 	IDDR |= 0xFF;
 	
 	CPORT = 0x0F; // 모든 자리수 선택
 	IPORT = ~0xFF; // 전체 세그먼트 ON ---> [8]
-		
-	data[3] = deciDigit % 10;
-	data[2] = (deciDigit / 10) % 10;
-	data[1] = (deciDigit / 100) % 10;
-	data[0] = deciDigit / 1000;
+	StandBy();
+	//st = 0;
+	IPORT = 0xFF;
 	
+	EIMSK |= (1 << INT0);
+	EICRA |= ((1 << ISC00) | (1 << ISC01));
+	//Timer0: 100ms stop-watch, Timer2: FMD refresh
+	TIMSK |= ((1 << OCIE0) | (1 << TOIE2));
+	TCCR0 |= ((1 << CS00) | (1 << CS01) | (1 << CS02)); // 분주비 1024
+	TCCR2 |= (1 << CS22); // 분주비 256(100) 1024(101) | (1/16M) * 256 * 256 = 0.004096s
+	OCR0 = 22; // (1/16M) * 22 * 1024 * 7 = 0.009856 (~10ms)
+	sei();
 	
-    /* Replace with your application code */
+	/* Replace with your application code */
     while (1) 
     {
-		/*
-		for (int i = 0; i < 4; i++) {
-			CPORT = 1 << i;
-			IPORT = ~0xFF;
-			for (int j = 0; j < 10; j++) {
-				IPORT = ~img[j];
-				_delay_ms(100);
+		/* 앞의 숫자 0은 출력 X (코드 최적화 필요)
+		for(int j = 0; j < 10000; j++) {
+			int r = digit(j);
+			if(r) {
+				for (int i = 0; i < sizeof(data); i++) { // 4자리 숫자 처리
+					CPORT = 1 << i;
+					if(j < 10) {
+						if(i == 0) {
+							IPORT = 0xFF;
+							continue;
+						}
+						if(i == 1) {
+							IPORT = 0xFF;
+							continue;
+						}
+						if(i == 2) {
+							IPORT = 0xFF;
+							continue;
+						}
+						IPORT = ~img[data[i]];
+					}
+					else if(j < 100) {
+						if(i == 0) {
+							IPORT = 0xFF;
+							continue;
+						}
+						if(i == 1) {
+							IPORT = 0xFF;
+							continue;
+						}
+						IPORT = ~img[data[i]];
+					}
+					else if(j < 1000) {
+						if(i == 0) {
+							IPORT = 0xFF;
+							continue;
+						}
+						IPORT = ~img[data[i]];
+					}
+					else {
+						IPORT = ~img[data[i]];
+					}
+					_delay_ms(1);
+				}
 			}
+			else {
+				break;
+			}
+			_delay_ms(10);
 		}
 		*/
-		for (int i = 0; i < sizeof(data); i++) { // 4자리 숫자 처리
-			CPORT = 1 << i;
-			IPORT = ~img[data[i]];
-			_delay_ms(1);
-		}
 	}
 }
 
